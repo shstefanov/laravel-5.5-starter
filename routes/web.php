2bfd6a2c1979f533e41153ef296581d80419c08c
Stefan Stefanov <stefan@localhost.localdomain>
2017-11-04 18:22:12 +0200

    formatting

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home',  'HomeController@index'        )->name('home'  );
 Route::get('/app',   'App\AppController@index'     )->name('app'   );
 Route::get('/panel', 'Panel\PanelController@index' )->name('panel' );
